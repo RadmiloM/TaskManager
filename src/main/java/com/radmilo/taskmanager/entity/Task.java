ce30f886686c22e5da7afbddd8f717c75841d5a7
RadmiloM <markovicradmilo35@gmail.com>
2024-01-13 22:12:38 +0100

    Constructor for title and description added.

--- src/main/java/com/radmilo/taskmanager/entity/Task.java
+++ src/main/java/com/radmilo/taskmanager/entity/Task.java
@@ -17,5 +17,8 @@ public class Task {
 
     private String description;
 
-
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
 }
