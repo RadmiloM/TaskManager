0c94bac61887d0ccc56505111247bb85e9c0797a
RadmiloM <markovicradmilo35@gmail.com>
2024-01-13 21:48:29 +0100

    Entity Task class created.

---
+++ src/main/java/com/radmilo/taskmanager/entity/Task.java
@@ -0,0 +1,21 @@
+package com.radmilo.taskmanager.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue
+    Long id;
+
+    private String title;
+
+    private String description;
+
+
+}
