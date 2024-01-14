4a7c2e7ddf8dda37c07bcef30dae9e48a0f96cbd
RadmiloM <markovicradmilo35@gmail.com>
2024-01-14 20:14:46 +0100

    NoArgsConstructor added to Task entity.

--- src/main/java/com/radmilo/taskmanager/entity/Task.java
+++ src/main/java/com/radmilo/taskmanager/entity/Task.java
@@ -13,9 +13,24 @@ public class Task {
     Long id;
     private String title;
     private String description;
+
     public Task(String title, String description) {
         this.title = title;
         this.description = description;
     }
 
+    public Task() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
