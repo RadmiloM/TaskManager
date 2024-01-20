a859a3b6b115571ac6fce87f6b43c973cee28f91
RadmiloM <markovicradmilo35@gmail.com>
2024-01-20 18:55:55 +0100

    TaskNotFoundException created.

---
+++ src/main/java/com/radmilo/taskmanager/exception/TaskNotFoundException.java
@@ -0,0 +1,9 @@
+package com.radmilo.taskmanager.exception;
+
+public class TaskNotFoundException extends RuntimeException {
+
+
+    public TaskNotFoundException(String message) {
+        super(message);
+    }
+}
