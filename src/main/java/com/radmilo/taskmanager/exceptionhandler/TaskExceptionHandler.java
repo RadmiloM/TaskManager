796b90e71e358883b1a1fe1e2883119780320577
RadmiloM <markovicradmilo35@gmail.com>
2024-01-21 18:24:40 +0100

    TaskExceptionHandler added handleMethodArgumentNotValid to handle client validation errors.

--- src/main/java/com/radmilo/taskmanager/exceptionhandler/TaskExceptionHandler.java
+++ src/main/java/com/radmilo/taskmanager/exceptionhandler/TaskExceptionHandler.java
@@ -1,11 +1,21 @@
 package com.radmilo.taskmanager.exceptionhandler;
 
 import com.radmilo.taskmanager.exception.TaskNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 public class TaskExceptionHandler extends ResponseEntityExceptionHandler {
@@ -14,4 +24,19 @@ public class TaskExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException taskNotFoundException) {
         return new ResponseEntity<>(taskNotFoundException.getMessage(), HttpStatus.NOT_FOUND);
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        List<Map<String, String>> taskErrors = new ArrayList<>();
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        taskErrors.add(errors);
+        return new ResponseEntity<>(taskErrors, HttpStatus.BAD_REQUEST);
+    }
+
 }
