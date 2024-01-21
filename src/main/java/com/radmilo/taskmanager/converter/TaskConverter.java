859016fcc73edb5a8b507c999d8281336acb1f44
RadmiloM <markovicradmilo35@gmail.com>
2024-01-21 19:45:58 +0100

    TaskConverter added method for converting from task list to taskdto list.

--- src/main/java/com/radmilo/taskmanager/converter/TaskConverter.java
+++ src/main/java/com/radmilo/taskmanager/converter/TaskConverter.java
@@ -4,6 +4,8 @@ import com.radmilo.taskmanager.dto.TaskDTO;
 import com.radmilo.taskmanager.entity.Task;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class TaskConverter {
 
@@ -16,5 +18,9 @@ public class TaskConverter {
 
     public TaskDTO mapToDTO(Task task) {
         return new TaskDTO(task.getTitle(), task.getDescription());
+    }
+
+    public List<TaskDTO> mapToDTO(List<Task> tasks) {
+        return tasks.stream().map(this::mapToDTO).toList();
     }
 }
