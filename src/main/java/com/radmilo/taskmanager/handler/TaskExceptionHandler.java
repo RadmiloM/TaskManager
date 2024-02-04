f0314fea1d3725c01a3ac8c3dd7fcc7544ccf9a3
RadmiloM <markovicradmilo35@gmail.com>
2024-02-04 13:06:32 +0100

    Deleted unnecessary extension for ResponseEntityExceptionHandler and updated method handleMethodArgumentNotValid.

--- src/main/java/com/radmilo/taskmanager/handler/TaskExceptionHandler.java
+++ src/main/java/com/radmilo/taskmanager/handler/TaskExceptionHandler.java
@@ -2,16 +2,12 @@ package com.radmilo.taskmanager.handler;
 
 import com.radmilo.taskmanager.error.Errors;
 import com.radmilo.taskmanager.exception.TaskNotFoundException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,18 +15,15 @@ import java.util.List;
 import java.util.Map;
 
 @ControllerAdvice
-public class TaskExceptionHandler extends ResponseEntityExceptionHandler {
+public class TaskExceptionHandler {
 
     @ExceptionHandler({TaskNotFoundException.class})
     public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException taskNotFoundException) {
         return new ResponseEntity<>(taskNotFoundException.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    @Override
-    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-                                                               HttpHeaders headers,
-                                                               HttpStatusCode status,
-                                                               WebRequest request) {
+    @ExceptionHandler
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
         List<Errors> taskErrors = new ArrayList<>();
         for (FieldError error : ex.getBindingResult().getFieldErrors()) {
             taskErrors.add(new Errors(error.getField(), error.getDefaultMessage()));
